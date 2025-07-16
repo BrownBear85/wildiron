ca3b6c9a56113d8f2a347ef30b46d8161384782e
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2025-07-16 09:34:17 -0700

    fixed irons mixin incompatibility

--- src/main/java/com/bonker/wildiron/mixin/LocalPlayerMixin.java
+++ src/main/java/com/bonker/wildiron/mixin/LocalPlayerMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(LocalPlayer.class)
+@Mixin(value = LocalPlayer.class, priority = 500) // lowered priority for compat with Iron's Spells 'n Spellbooks
 public class LocalPlayerMixin {
     @Redirect(method = {"aiStep", "canStartSprinting"},
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isUsingItem()Z"))
